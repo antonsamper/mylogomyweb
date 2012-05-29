408be6dafa3f8fd9eebdd28f7587806e72f7e836
Anton Samper Rivaya <anton@aeser.co.uk>
2012-05-29 17:26:10 +0100

    formatting

--- scripts/sizeToBytes.js
+++ scripts/sizeToBytes.js
@@ -17,5 +17,5 @@ function sizeToBytes(size, format) {
 
 //Example Calls
 sizeToBytes(10,'MB'); //10485760
-sizeToBytes(1,'KB'); //1024
-sizeToBytes(1,'TB'); //1099511627776
+sizeToBytes(1,'KB');  //1024
+sizeToBytes(1,'TB');  //1099511627776
